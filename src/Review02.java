93018f9502891f868e5c22a214dd481af4b779ee
ryoutakimura <yellow11053@gmail.com>
2023-11-12 15:50:17 +0900

    課題３プログラム作成

--- src/Review02.java
+++ src/Review02.java
@@ -2,8 +2,27 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
+        int i;
+        boolean flag=true; //n倍判定フラグ
+        //１から１００まで
+        for(i = 1;i <= 100;i++) {
+            //３倍判定のとき「Fizz」と出力する
+            if(i % 3 == 0) {
+                System.out.print("Fizz");
+                flag=false;
+            }
+            //5倍判定のとき「Buzz」と出力する
+            if(i % 5 == 0) {
+                System.out.print("Buzz");
+                flag=false;
+            }
+            //ｎ倍の判定に入らなかった場合整数を出力する
+            if(flag) {
+                System.out.print(i);
+            }
+            System.out.println(""); //改行
+            flag=true; //ｎ倍判定リセット
+        }
     }
 
 }
